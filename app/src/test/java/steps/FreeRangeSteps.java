0c7bca2867df3909fe76ed7f82bc93f360636fd6
German Vazquez <german.avc@gmail.com>
2024-09-13 12:59:24 -0600

    Se agregan ejemplos de Assertions y SOFT assertions, para entender la sintaxis.

--- app/src/test/java/steps/FreeRangeSteps.java
+++ app/src/test/java/steps/FreeRangeSteps.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
@@ -15,6 +16,8 @@ import pages.PaginaPrincipal;
 import pages.PaginaRegistro;
 
 public class FreeRangeSteps {
+
+    SoftAssert soft = new SoftAssert();
 
     PaginaPrincipal landingPage = new PaginaPrincipal();
     PaginaCursos cursosPage = new PaginaCursos();
@@ -52,8 +55,42 @@ public class FreeRangeSteps {
         List<String> lista = registro.returnPlanDropdownValues();
         List<String> listaEsperada = Arrays.asList("Academia: $16.99 / mes • 13 productos",
                 "Academia: $300 / año • 13 productos", "Free: Gratis • 3 productos");
-        
+
         Assert.assertEquals(listaEsperada, lista);
+    }
+
+    public void EjemploAssertions() {
+        // assertEquals - Verifica que dos valores sean iguales.
+
+        String actualTitle = "FreeRangeToaster";
+        String expectedTitle = "FreeRangeTester";
+        String incorrectTitle = "algo";
+        Boolean isElementPresent = true;
+
+        Assert.assertEquals(actualTitle, expectedTitle, "El título de la página no es el esperado.");
+
+        // assertNotEquals - Verifica que dos valores no sean iguales.
+
+        Assert.assertNotEquals(actualTitle, incorrectTitle, "El título de la página no debería ser este.");
+
+        // assertTrue - Verifica que una condición sea verdadera.
+
+        Assert.assertTrue(isElementPresent, "El elemento debería estar presente.");
+
+        // assertFalse - Verifica que una condición sea falsa.
+
+        Assert.assertFalse(isElementPresent, "El elemento no debería estar presente.");
+
+        String palabraEsperada = "Pepe";
+        String palabraEncontrada = "Papa";
+
+        // Soft Assertions: No detienen la ejecución al fallar. Ideal para verificar
+        // muchas cosas pequeñas a la vez.
+        soft.assertEquals(palabraEsperada, palabraEncontrada, "El título de la página no es el esperado.");
+        soft.assertTrue(palabraEncontrada.contains(palabraEsperada), "El elemento debería estar presente");
+        soft.assertNotEquals(palabraEncontrada, palabraEsperada, "El título de la página no debería ser este.");
+
+        soft.assertAll();
     }
 
 }
